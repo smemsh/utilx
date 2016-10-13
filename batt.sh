ff051d4b21eb8d5298d03788c3cfdd1a7c4d052e
Scott Mcdermott <scott@smemsh.net>
2016-10-13 00:45:09 -0700

    + new: batt.sh: batt, hibernate, resume, suspend
    wraps common power management functions on portable workstation role

---
+++ batt.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+#
+# batt.sh: batt, hibernate, suspend
+#   simple power management
+#
+# desc:
+#   - batt: how much battery is left (acpi is useless on X61s)
+#   - hibernate: hibernates machine after optionally invoking afk
+#   - suspend: suspends machine after optionally invoking afk
+#
+# scott@smemsh.net
+# http://smemsh.net/src/ratutils/
+# http://spdx.org/licenses/GPL-2.0
+#
+##############################################################################
+
+set -e
+
+invname=${0##*/}
+
+###
+
+maybe_go_afk ()
+{
+	if [[ $(type afk &>/dev/null) ]]
+	then read -sn 1 -p "go afk first (y/n/a)? "
+	else return; fi
+
+	[[ $REPLY == y ]] && { echo "going afk first"; afk; return 0; }
+	[[ $REPLY == n ]] && { echo "not going afk first"; return 0; }
+	[[ $REPLY == a ]] && { echo "aborted"; exit 1; }
+
+	echo no such reply
+	exit 4
+}
+
+display_battery_status ()
+{
+	# these utils don't seem to work or be available flags
+	#acpitool -b
+	#acpitool -B
+
+	export BC_ENV_ARGS='-ql'
+
+	cd /sys/class/power_supply/BAT0
+	batt=$(echo "scale = 30; ($(<energy_now)/$(<energy_full)) * 100" | bc)
+	batt=${batt%.*}
+	echo "$batt%"
+}
+
+###
+
+main ()
+{
+	if ((`id -u` == 0)); then
+		echo do not run as root
+		exit 1
+	fi
+
+	case $invname in
+
+	(hibernate)	which=hibernate;;
+	(suspend)	which=suspend;;
+	(battery|batt)	display_battery_status;				exit;;
+	(*)		echo "$invname: unimplemented"; false;		exit;;
+
+	esac
+
+	maybe_go_afk && echo
+	printf "%s""ing..." ${which%e} # hibernateing -> hibernating
+	pm-$which
+	echo back
+}
+
+main "$@"
